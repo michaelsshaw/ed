75a6fa640862b76fe74ebcd309cc43733984b7a4
Michael Shaw <michaelsshaw44@gmail.com>
2024-01-23 17:28:52 -0500

    handle SIGINT

--- ed.c
+++ ed.c
@@ -1,8 +1,16 @@
 /* SPDX-License-Identifier: MIT */
 #include <stdio.h>
+#include <signal.h>
+
+void sigint(int signo)
+{
+	puts("\n?");
+}
 
 int main()
 {
+	signal(SIGINT, sigint);
+
 	while (1) {
 		char c = getchar();
 		if (c == '\n')
